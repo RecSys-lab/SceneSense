6777fd798cc49f38ecda09b84948922965255c2e
Ali Tourani <a.tourani1991@gmail.com>
2024-05-08 17:10:00 +0200

    [feat] - reading the config file

--- main.py
+++ main.py
@@ -1,7 +1,14 @@
 #!/usr/bin/env python3
-import yaml
+from src.utils import readConfigs
 
 def main():
-    print("Welcome to the SceneSense Framework!")
+    print("Welcome! Starting 'SceneSense'!\n")
+    # Read the configuration file
+    configs = readConfigs("config/config.yml")
+    # If properly read, print the configurations
+    if configs:
+        print(configs)
+    # Finish the program
+    print("\nStopping 'SceneSense'!")
 
 main()
---
+++ src/utils.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+import yaml
+
+def readConfigs(configPath: str):
+    """
+    Read the configuration file and store the values in a dictionary
+
+    Parameters
+    -------
+    configPath: str
+        The path to the configuration file
+
+    Returns
+    -------
+    windowTitle: str
+        The name of the window to be shown
+    """
+    with open(configPath) as cfg:
+        try:
+            print("Reading the configuration file...")
+            return yaml.safe_load(cfg)
+        except yaml.YAMLError as err:
+            print(f"[Error] Error while reading the configuration file: {err}")
