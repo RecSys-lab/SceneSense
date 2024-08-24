e2fc6cab6fcb9ecd1caa0334e50d4ad2b87bda7b
Ali Tourani <a.tourani1991@gmail.com>
2024-08-24 12:58:32 +0200

    [feat] - improve features

--- src/core/pipeline/visual_features/utils.py
+++ src/core/pipeline/visual_features/utils.py
@@ -1,7 +1,9 @@
 import os
+import time
 import string
 import cv2 as cv
 import numpy as np
+import pandas as pd
 from glob import glob
 
 def initMovieFramesFolders(configs: dict):
@@ -78,12 +80,54 @@ def initFeaturesFolder(framesDir: str, featuresDir: str):
         os.mkdir(generatedPath)
         return generatedPath
 
+def featureExtractor(imageFile, model, preProcess, inputSize: int):
+    """
+    Pre-checks and generates the output visual features folder
+
+    Parameters
+    ----------
+    framesDir: str
+        The frames folder address to extract visual features from
+    featuresDir: str
+        The visual features directory to save the extracted features
+    
+    Returns
+    -------
+    videoFiles: list
+        A list of fetched video files
+    """
+    # Imports
+    from tensorflow.keras.preprocessing.image import load_img, img_to_array
+    # Variables
+    features = None
+    try:
+        # Extracting the content of the image
+        imageContent = load_img(imageFile, target_size=(inputSize, inputSize))
+        # Convert the image pixels to a numpy array
+        frameData = img_to_array(imageContent)
+        frameData = np.expand_dims(frameData, axis=0)
+        # Preprocessing
+        frameData = preProcess(frameData)
+        # Get extracted features
+        features = model.predict(frameData)
+        # Return the extracted features
+        return features
+    except Exception as error:
+        print(f'- Error while extracting the features of "{imageFile}": {str(error)}')
+        return None
+
 def modelRunner(model, framesFolder, outputDir, configs: dict):
     """
     Pre-checks the given directory for movie frames and prepares it for further processing
 
     Parameters
     ----------
+    model: Model
+        The initialized model for feature extraction
+    framesFolder: str
+        The frames folder address to extract visual features from
+    outputDir: str
+        The visual features directory to save the extracted features
     configs: dict
         The configurations dictionary
     
@@ -93,17 +137,51 @@ def modelRunner(model, framesFolder, outputDir, configs: dict):
         A list of fetched frames folders
     """
     # Variables
+    packetIndex = 1  # Holds the name of the packet, e.g. Packet0001
+    packetCounter = 0
+    modelInputSize = 0
+    modelPreprocess = None
+    startTime = time.time()
+    imageTypes = configs['image_formats']
+    totalFrames = len(os.listdir(framesFolder))
     modelName = configs['feature_extractor_model']
+    remainingFrames = len(os.listdir(framesFolder))
+    frameFeatureDF = pd.DataFrame(columns=['frameId', 'features'])
+    # Prepare the model-specific variables
     if modelName == 'incp3':
         # Load proper imports
         from tensorflow.keras.applications.inception_v3 import preprocess_input
         # Load Inception-v3 model variables
-        inputSize = 299  # Default input size for Inception-v3 model
+        modelInputSize = 299  # Default input size for Inception-v3 model
+        modelPreprocess = preprocess_input
     elif modelName == 'vgg19':
         # Load proper imports
         from tensorflow.keras.applications.vgg19 import preprocess_input
         # Load VGG-19 model variables
-        inputSize = 224 # Default input size for Inception-v3 model
+        modelInputSize = 224 # Default input size for Inception-v3 model
+        modelPreprocess = preprocess_input
     else:
         print(f"Feature extraction model '{modelName}' is not supported! Exiting ...")
         return
+    # Loop over the frames in the folder
+    for imageType in imageTypes:
+        for frameFile in glob(f'{framesFolder}/*.{imageType}'):
+            # Variables
+            frameFileName = os.path.basename(frameFile)
+            framesFolder = os.path.basename(os.path.dirname(frameFile))
+            try:
+                # Finding frameId by removing .jpg from the name
+                frameId = ('frame' + frameFile.rsplit('frame', 1)[1])[:-4]
+                # Get the extracted features
+                features = featureExtractor(frameFile, model, modelPreprocess, modelInputSize)
+                # Check the extracted features
+                if features is None:
+                    print(f'- No features extracted! Skipping "{frameFileName}" in "{framesFolder}" ...')
+                    continue
+            except Exception as error:
+                print(f'- Error while extracting the features of "{frameFileName}" in "{framesFolder}": {str(error)}')
+                continue
+    # Inform the user about the extraction process
+    elapsedTime = '{:.2f}'.format(time.time() - startTime)
+    print(
+        f'- Extracted {totalFrames} features ({packetIndex-1} packets) of "{framesFolder}" in {elapsedTime} seconds!')
